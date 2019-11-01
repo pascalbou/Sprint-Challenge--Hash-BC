bd67fbe682ebada3db91f700ed0777de39fcb34c
krikaz <pascal.bou@protonmail.com>
2019-11-01 10:42:27 +0100

    coded valid proof

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -39,8 +39,13 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    # set a guess
+    guess = f'{last_hash}{proof}'.encode()
+    # hash the guess
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    # return guess validity
+    return guess_hash[:6] == last_hash[:6]
 
 
 if __name__ == '__main__':
